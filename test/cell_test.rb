4a763c9258c838344da960cd3c60ae1a5305d2fe
Eugene Theriault <hybridbassist@gmail.com>
2020-08-24 19:01:59 -0700

    Create cell class and initialize test

---
+++ test/cell_test.rb
@@ -0,0 +1,11 @@
+require './test/test_helper'
+
+class CellTest < Minitest::Test
+  def setup
+    @cell = Cell.new("B4")
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+end
