aa6aab6567f1ba18b321ed3738ec9ebf4126fa84
Eugene Theriault <hybridbassist@gmail.com>
2020-08-27 16:16:11 -0700

    Add main menu to game

--- lib/game.rb
+++ lib/game.rb
@@ -10,4 +10,19 @@ class Game
     @board_2 = Board.new
   end
 
+  def main_menu
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit"
+    input = gets.chomp
+    if input.downcase == "p"
+      # go to method that starts game
+    elsif input.downcase == "q"
+      abort "Okay, bye!"
+    else
+      puts "Please enter p or q"
+      main_menu
+    end
+  end
+
+
 end
