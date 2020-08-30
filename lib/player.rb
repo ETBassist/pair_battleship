8fe29ff9402ae474ce88f6174d25b689131803ac
Eugene Theriault <hybridbassist@gmail.com>
2020-08-30 15:00:27 -0700

    Add has_lost? method and test to player

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,20 @@
 class Player
-  attr_reader :ships
+  attr_reader :ships, :board, :last_shot
 
   def initialize
     @board = nil
     @ships = []
     @last_shot = nil
   end
+
+  def add_ship(ship)
+    @ships << ship
+  end
+
+  def has_lost?
+    @ships.all? do |ship|
+      ship.sunk?
+    end
+  end
+
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -13,4 +13,26 @@ class PlayerTest < Minitest::Test
     assert_equal [], @player.ships
   end
 
+  def test_it_can_add_ships
+    cruiser = Ship.new("Cruiser", 3)
+    @player.add_ship(cruiser)
+    assert_equal [cruiser], @player.ships
+  end
+
+  def test_has_lost_when_total_ships_health_zero
+    cruiser = Ship.new("Cruiser", 3)
+    destroyer = Ship.new("Destroyer", 2)
+    @player.add_ship(cruiser)
+    @player.add_ship(destroyer)
+    assert_equal false, @player.has_lost?
+    destroyer.hit
+    destroyer.hit
+    cruiser.hit
+    cruiser.hit
+    cruiser.hit
+    assert_equal true, destroyer.sunk?
+    assert_equal true, cruiser.sunk?
+    assert_equal true, @player.has_lost?
+  end
+
 end
