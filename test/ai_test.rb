f8473484e0f9375d34136699c2fad1b59a52a844
Eugene Theriault <hybridbassist@gmail.com>
2020-09-01 16:50:54 -0700

    Add test to check that ai has ship bucket array

--- test/ai_test.rb
+++ test/ai_test.rb
@@ -3,10 +3,19 @@ require './test/test_helper'
 class AITest < Minitest::Test
   def setup
     @ai = AI.new
+    @ai_player = Player.new
+    @board = Board.new
+    @ai_player.board = @board
+    @ship = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
     assert_instance_of AI, @ai
+    assert_instance_of Board, @ai_player.board
+  end
+
+  def test_ship_bucket_is_array
+    assert_equal [], @ai.ai_ship_bucket
   end
 
 end
